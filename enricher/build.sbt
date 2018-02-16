5d2b9693093711c8510279ca3ab83117bd6efebc
Valery.Malakhov <Valery.Malakhov@sigma.software>
2018-02-16 13:49:08 +0200

    Cosmetic fixes.

--- enricher/build.sbt
+++ enricher/build.sbt
@@ -1,39 +1,28 @@
 import Dependencies._
 import sbt.Keys._
 
-lazy val enricher = (project in file("."))
+lazy val enricher = project.in(file("."))
   .settings(
     name := "LogsEnricher",
-    version := "1.0.0.f4",
-    scalaVersion := scala211,
-    mainClass in assembly := Some("com.prb.dnhs.MainApp"),
-    mainClass in(Compile, run) := Some("com.prb.dnhs.MainApp"),
-    fullClasspath in Runtime := (fullClasspath in Compile).value,
-    test in assembly := {}
+    version := "1.0.0.f3",
+    scalaVersion := "2.11.11"
   )
-
-exportJars := true
-fork := true
-
-libraryDependencies ++= Seq(
-  sparkCore, sparkSQL, sparkStreaming, sparkHive,
-  scalazStream, scalazCore,
-  parquetColumn,
-  configType, scopt,
-  specs2, specs2Mock, scalatest,
-  slf4j, cats
-)
-
-scalacOptions += "-Ypartial-unification"
-
-enablePlugins(AssemblyPlugin)
-
-// Assembly added for correct work with scopt, slf4j and configType.
-assemblyMergeStrategy in assembly := {
-  case PathList("META-INF", xs@_ *) => MergeStrategy.discard
-  case n if n.endsWith(".conf") => MergeStrategy.concat
-  case x => MergeStrategy.last
-}
-
-assemblyJarName := "LogsEnricher.jar"
+  .settings(
+    mainClass in(Compile, run) := Some("com.prb.dnhs.MainApp"),
+    fullClasspath in Runtime := (fullClasspath in Compile).value
+  )
+  .settings(
+    libraryDependencies ++=
+      spark ++
+      sTest ++
+      Seq(
+        scalazStream
+        , scalazCore
+        , parquetColumn
+        , configType
+        , scopt
+        , slf4j
+        , cats
+      )
+  )
 
--- enricher/project/Dependencies.scala
+++ enricher/project/Dependencies.scala
@@ -1,52 +1,55 @@
+import sbt.Keys._
 import sbt._
 
+//==============Versions===============
+object V {
+  lazy val sparkVersion					= "2.2.0"
+  lazy val hiveVersion					= "2.3.2"
+  lazy val parquetVersion				= "1.9.0"
+  lazy val scalazStreamVersion	= "0.8.6"
+  lazy val scalazCoreVersion		= "7.2.18"
+  lazy val typesafeVersion			= "1.3.2"
+  lazy val scoptVersion					= "3.7.0"
+  lazy val slf4jVersion					= "3.7.2"
+  lazy val catsVersion					= "1.0.1"
+  lazy val specs2Version				= "4.0.2"
+  lazy val scalatestVersion			= "3.0.5"
+}
+
 object Dependencies {
+  lazy val sparkCore			= "org.apache.spark" %% "spark-core"			% V.sparkVersion
+  lazy val sparkSQL				= "org.apache.spark" %% "spark-sql"				% V.sparkVersion
+  lazy val sparkStreaming	= "org.apache.spark" %% "spark-streaming"	% V.sparkVersion
+  lazy val sparkHive			= "org.apache.spark" %% "spark-hive"			% V.sparkVersion
 
-  //==============Versions===============
+  lazy val spark = Seq(
+    sparkCore
+    , sparkSQL
+    , sparkStreaming
+    , sparkHive
+  )
 
-  lazy val scala211 = "2.11.11"
-  lazy val scala212 = "2.12.4"
+  lazy val parquetColumn	= "org.apache.parquet" % "parquet-column"	% V.parquetVersion
 
-  lazy val sparkVersion = "2.2.0"
-  lazy val hiveVersion = "2.3.2"
-  lazy val parquetVersion = "1.9.0"
+  lazy val scalazStream		= "org.scalaz.stream" %% "scalaz-stream"	% V.scalazStreamVersion
+  lazy val scalazCore			= "org.scalaz" %% "scalaz-core"						% V.scalazCoreVersion
 
-  lazy val scalazStreamVersion = "0.8.6"
-  lazy val scalazCoreVersion = "7.2.18"
+  lazy val configType	= "com.typesafe" % "config"			% V.typesafeVersion
+  lazy val slf4j			= "com.typesafe.scala-logging" %% "scala-logging" % V.slf4jVersion
 
-  lazy val typesafeVersion = "1.3.2"
-  lazy val scoptVersion = "3.7.0"
+  lazy val scopt		= "com.github.scopt" %% "scopt"		% V.scoptVersion
 
-  lazy val specs2Version = "4.0.2"
-  lazy val scalaMockVersion = "4.0.0"
-  lazy val scalatestVersion = "3.0.5"
+  lazy val cats			= "org.typelevel" %% "cats-core"	% V.catsVersion
 
-  lazy val slf4jVersion = "3.7.2"
+  //  Tests  //
+  lazy val specs2			= "org.specs2" %% "specs2-core"		% V.specs2Version			% "test"
+  lazy val specs2Mock	= "org.specs2" %% "specs2-mock"		% V.specs2Version			% "test"
+  lazy val scalatest	= "org.scalatest" %% "scalatest"	% V.scalatestVersion	% "test"
 
-  lazy val catsVersion = "1.0.1"
-
-  //============Dependencies=============
-
-  lazy val sparkCore = "org.apache.spark" %% "spark-core" % sparkVersion
-  lazy val sparkSQL = "org.apache.spark" %% "spark-sql" % sparkVersion
-  lazy val sparkStreaming = "org.apache.spark" %% "spark-streaming" % sparkVersion
-  lazy val sparkHive = "org.apache.spark" %% "spark-hive" % sparkVersion
-
-  lazy val parquetColumn = "org.apache.parquet" % "parquet-column" % parquetVersion
-
-  lazy val scalazStream = "org.scalaz.stream" %% "scalaz-stream" % scalazStreamVersion
-  lazy val scalazCore = "org.scalaz" %% "scalaz-core" % scalazCoreVersion
-
-  lazy val configType = "com.typesafe" % "config" % typesafeVersion
-  lazy val slf4j = "com.typesafe.scala-logging" %% "scala-logging" % slf4jVersion
-
-  lazy val scopt = "com.github.scopt" %% "scopt" % scoptVersion
-
-  lazy val specs2 = "org.specs2" %% "specs2-core" % specs2Version % "test"
-  lazy val specs2Mock = "org.specs2" %% "specs2-mock" % specs2Version % "test"
-
-  lazy val scalatest = "org.scalatest" %% "scalatest" % scalatestVersion % "test"
-
-  lazy val cats = "org.typelevel" %% "cats-core" % catsVersion
+  lazy val sTest = Seq(
+      specs2
+    , specs2Mock
+    , scalatest
+  )
 }
 
